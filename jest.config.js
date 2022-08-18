1d3cd028ffbf0eb101f06f6f185a26c2cf4370a7
Nicholas Jean <nicholas.jean@lcecorp.com>
2022-08-18 10:45:48 -0400

    chore - fixing tsconfigs and making a gatsby application

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,5 @@
 module.exports = {
   projects: [
-    '<rootDir>/apps/gatsby-demo',
     '<rootDir>/libs/test-package-main',
     '<rootDir>/libs/test-package-one',
     '<rootDir>/libs/test-package-two',
